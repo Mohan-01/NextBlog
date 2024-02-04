de17c6a86c06af745966b3fb938642bc6e849136
M. Mohan <20501a05c1@pvpsit.ac.in>
2024-02-04 23:18:03 +0530

    Added spped insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import Layout from "../components/layout/layout";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <SpeedInsights />
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
